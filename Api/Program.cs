1558a6a8507e11381f74afaf19443c1c7e73e982
CihatAydın <cihataydin601@gmail.com>
2021-06-16 14:25:28 +0300

    Edited Dockerfile and http url configuration

--- Api/Program.cs
+++ Api/Program.cs
@@ -20,6 +20,7 @@ namespace Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:1000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
